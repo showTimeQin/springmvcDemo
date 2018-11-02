b2d78bb9ff16d35494caaaf30b411d9712630e0d
andy <594470882@qq.com>
2018-11-02 17:52:17 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/MyMvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.example.demo")
+public class MyMvcConfig {
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/classes/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+}
